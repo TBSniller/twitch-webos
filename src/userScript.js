b2cf7cac64421108a9e72fe0297e543806426244
TBSniller <tbsniller@gmail.com>
2022-01-02 17:05:28 +0100

    Skeleton to visit twitch.tv in app

---
+++ src/userScript.js
@@ -0,0 +1,12 @@
+import 'whatwg-fetch';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import './domrect-polyfill';
+
+import {handleLaunch} from './utils';
+
+document.addEventListener('webOSRelaunch', (evt) => {
+  console.info('RELAUNCH:', evt, window.launchParams);
+  handleLaunch(evt.detail);
+}, true);
+
---
+++ src/utils.js
@@ -0,0 +1,31 @@
+
+export function extractLaunchParams() {
+    if (window.launchParams) {
+      return JSON.parse(window.launchParams);
+    } else {
+      return {};
+    }
+  }
+  
+  export function handleLaunch(params) {
+    // We use our custom "target" param, since launches with "contentTarget"
+    // parameter do not respect "handlesRelaunch" appinfo option. We still
+    // fallback to "contentTarget" if our custom param is not specified.
+    //
+    let { target, contentTarget = target } = params;
+  
+  
+    if (contentTarget && typeof contentTarget === 'string') {
+  
+      if (contentTarget.indexOf('https://m.twitch.tv/') === 0) {
+        console.info('Launching from direct contentTarget:', contentTarget);
+        window.location = contentTarget;
+      } else {
+        console.info('Launching from partial contentTarget:', contentTarget);
+        window.location = 'https://m.twitch.tv/' + contentTarget;
+      }
+    } else {
+      console.info('Default launch');
+      window.location = 'https://m.twitch.tv/';
+    }
+  }
